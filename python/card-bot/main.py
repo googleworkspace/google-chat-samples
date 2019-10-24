8c6aa5a3160812d9302e1955e9e936700b80b1ce
Steven Bazyl <sbazyl@google.com>
2019-10-24 14:15:16 -0600

    Update to python 3.7, remove async code to align w/other languages (have separate sample for async)

--- python/card-bot/main.py
+++ python/card-bot/main.py
@@ -11,12 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
+# pylint: disable=invalid-name
+"""
+Hangouts Chat bot that responds to events and messages from a room
+synchronously. The bot formats the response using cards,
+inserting widgets based upon the user's original input.
+"""
 import logging
-from apiclient.discovery import build, build_from_document
-from flask import Flask, render_template, request, json, make_response
-from httplib2 import Http
-from oauth2client.service_account import ServiceAccountCredentials
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
@@ -24,6 +27,7 @@ INTERACTIVE_TEXT_BUTTON_ACTION = "doTextButtonAction"
 INTERACTIVE_IMAGE_BUTTON_ACTION = "doImageButtonAction"
 INTERACTIVE_BUTTON_PARAMETER_KEY = "param_key"
 BOT_HEADER = 'Card Bot Python'
+
 
 @app.route('/', methods=['POST'])
 def home_post():
@@ -40,16 +44,16 @@ def home_post():
     # If the bot is removed from the space, it doesn't post a message
     # to the space. Instead, log a message showing that the bot was removed.
     if event_data['type'] == 'REMOVED_FROM_SPACE':
-        logging.info('Bot removed from  %s' % event_data['space']['name'])
+        logging.info('Bot removed from  %s', event_data['space']['name'])
         return 'OK'
 
-    elif event_data['type']  == 'ADDED_TO_SPACE' and event_data['space']['type'] == 'ROOM':
-        resp = { 'text': ('Thanks for adding me to {}!'
-            .format(event_data['space']['name'])) }
+    if event_data['type'] == 'ADDED_TO_SPACE' and event_data['space']['type'] == 'ROOM':
+        resp = {'text': ('Thanks for adding me to {}!'
+                         .format(event_data['space']['name']))}
 
-    elif event_data['type']  == 'ADDED_TO_SPACE' and event_data['space']['type'] == 'DM':
-        resp = { 'text': ('Thanks for adding me to a DM, {}!'
-            .format(event_data['user']['displayName'])) }
+    elif event_data['type'] == 'ADDED_TO_SPACE' and event_data['space']['type'] == 'DM':
+        resp = {'text': ('Thanks for adding me to a DM, {}!'
+                         .format(event_data['user']['displayName']))}
 
     elif event_data['type'] == 'MESSAGE':
         resp = create_card_response(event_data['message']['text'])
@@ -59,21 +63,8 @@ def home_post():
         parameters = event_data['action']['parameters']
         resp = respond_to_interactive_card_click(action_name, parameters)
 
-    space_name = event_data['space']['name']
-
     logging.info(resp)
-
-    # Uncomment the following line for a synchronous response.
-    #return json.jsonify(resp)
-
-    # Asynchronous response version:
-    thread_id = None
-    if event_data['message']['thread'] != None:
-        thread_id = event_data['message']['thread']
-
-    # Need to return a response to avoid an error in the Flask app
-    send_async_response(resp, space_name, thread_id)
-    return 'OK'
+    return json.jsonify(resp)
 
 @app.route('/', methods=['GET'])
 def home_get():
@@ -85,31 +76,6 @@ def home_get():
 
     return render_template('home.html')
 
-
-def send_async_response(response, space_name, thread_id):
-    """Sends a response back to the Hangouts Chat room asynchronously.
-
-    Args:
-      response: the response payload
-      spaceName: The URL of the Hangouts Chat room
-
-    """
-
-    # The following two lines of code update the thread that raised the event.
-    # Delete them if you want to send the message in a new thread.
-    if thread_id != None:
-        response['thread'] = thread_id
-    ##################################
-
-    scopes = ['https://www.googleapis.com/auth/chat.bot']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        'service-acct.json', scopes)
-    http_auth = credentials.authorize(Http())
-
-    chat = build('chat', 'v1', http=http_auth)
-    chat.spaces().messages().create(
-        parent=space_name,
-        body=response).execute()
 
 def create_card_response(event_message):
     """Creates a card response based on the message sent in Hangouts Chat.
@@ -143,7 +109,7 @@ def create_card_response(event_message):
 
         elif word == 'textparagraph':
             widgets.append({
-                'textParagraph' : {
+                'textParagraph': {
                     'text': '<b>This</b> is a <i>text paragraph</i>.'
                 }
             })
@@ -242,15 +208,14 @@ def create_card_response(event_message):
                 }
             })
 
-
-
-    if header != None:
+    if header is not None:
         cards.append(header)
 
-    cards.append({ 'sections': [{ 'widgets': widgets }]})
+    cards.append({'sections': [{'widgets': widgets}]})
     response['cards'] = cards
 
     return response
+
 
 def respond_to_interactive_card_click(action_name, custom_params):
     """Creates a response for when the user clicks on an interactive card.
@@ -265,7 +230,7 @@ def respond_to_interactive_card_click(action_name, custom_params):
     """
     message = 'You clicked {}'.format(
         'a text button' if action_name == INTERACTIVE_TEXT_BUTTON_ACTION
-            else 'an image button')
+        else 'an image button')
 
     original_message = ""
 
@@ -312,3 +277,9 @@ def respond_to_interactive_card_click(action_name, custom_params):
             }
         ]
     }
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
