688ef52f71afa755a2bb2508e1c1364558f83cce
Steven Bazyl <sbazyl@google.com>
2020-01-28 15:46:50 -0700

    Revise comments/formatting based on review, simplify JSON access

--- java/basic-bot/src/main/java/com/google/chat/bot/basic/Bot.java
+++ java/basic-bot/src/main/java/com/google/chat/bot/basic/Bot.java
@@ -13,25 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Copyright 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.google.chat.bot.basic;
 
 // [START basic-bot]
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.api.services.chat.v1.model.Message;
 import java.util.logging.Logger;
@@ -52,7 +36,7 @@ public class Bot {
   }
 
   /**
-   * Handles a GET request to the /bot endpoint.
+   * Returns a simple text message bot response based on the type of triggering event
    *
    * @param event Event from chat.
    * @return Message
@@ -63,27 +47,25 @@ public class Bot {
     String replyText = "";
     switch (event.get("type").asText()) {
       case "ADDED_TO_SPACE":
-        JsonNode space = event.get("space");
-        String spaceType = space.get("type").asText();
+        String spaceType = event.at("/space/type").asText();
         if ("ROOM".equals(spaceType)) {
-          String displayName = space.get("displayName").asText();
+          String displayName = event.at("/space/displayName").asText();
           replyText = String.format("Thanks for adding me to %s", displayName);
         } else {
-          String displayName = event.get("user").get("displayName").asText();
+          String displayName = event.at("/user/displayName").asText();
           replyText = String.format("Thanks for adding me to a DM, %s!", displayName);
         }
         break;
       case "MESSAGE":
-        String message = event.get("message").get("text").asText();
+        String message = event.at("/message/text").asText();
         replyText = String.format("Your message: %s", message);
         break;
       case "REMOVED_FROM_SPACE":
-        String name = event.get("space").get("name").asText();
+        String name = event.at("/space/name").asText();
         logger.info(String.format("Bot removed from %s", name));
         break;
       default:
         replyText = "Cannot determine event type";
-        break;
     }
     Message reply = new Message().setText(replyText);
     return reply;
