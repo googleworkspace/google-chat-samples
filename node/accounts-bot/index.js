4ca95b90bbe78345773eeb0741e650e1b454e947
asrivas <asrivastava274@gmail.com>
2020-02-22 21:38:24 -0500

    Checks account names from data sheet

--- node/accounts-bot/index.js
+++ node/accounts-bot/index.js
@@ -14,19 +14,24 @@ exports.accountsBot = async (req, res) => {
     const message = req.query.message || req.body.message;
     if (!message || !message.text) {
       console.log('invalid input');
-      res.send('Please provide an account to look up');
+      res.send(createTextResponse('Please provide an account to look up'));
     }
 
-    const accountName = JSON.stringify(message.text);
-    const sheetsClient = getSheetsClient();
-    res.send('finished');
+    const accountName = getAccountName(JSON.stringify(message.text));
+    console.log('accountName: ' + accountName);
+    const sheetsClient = await getSheetsClient();
+    var ownerData = await accountLookup(sheetsClient, accountName);
+    if (ownerData.length > 0) {
+
+    }
+    res.send(createTextResponse(ownerData));
 }
 
 /**
  * Looks up ownership information for the account.
  *
  * @param {String} accountName inbound message
- * @return {String} owner name
+ * @return {String} owner data
  */
 async function accountLookup(client, accountName) {
     console.log(`account: ${accountName}`);
@@ -34,16 +39,19 @@ async function accountLookup(client, accountName) {
       spreadsheetId: ACCOUNTS_SHEET_ID,
       range: 'Sheet1!A:D',
     });
-    let accountTable = accountsSheet.data.values;
-    console.log(`accounts: ${JSON.stringify(accountTable, null, 4)}`);
-    var exists = false;
+    let accountsTable = accountsSheet.data.values;
+    console.log(`accounts: ${JSON.stringify(accountsTable, null, 4)}`);
 
-    if (exists) {
-        // return card with info
-        return 'There is an owner';
-    }
-    // return card with please update message.
-    return 'The account specified could not be found.';
+    var data = [];
+
+    accountsTable.forEach(entry => {
+        console.log('entry: ' + entry);
+        if (entry[0] == accountName) {
+            exists = true;
+            data = entry;
+        }
+    });
+    return data;
   }
 
 /**
@@ -57,4 +65,33 @@ async function getSheetsClient() {
       scopes: [SHEETS_SCOPE],
     });
     return google.sheets({version: 'v4', auth});
+  }
+
+/**
+ * Creates JSON response for a simple text message for Hangouts.
+ *
+ * @param {String} message the text of message response
+ * @return {JSON} the reponse data
+*/
+function createTextResponse(message) {
+    const data = JSON.stringify({
+      'text': message,
+    });
+    return data;
+}
+
+  /**
+ * Gets the last word in the text and removes punctuation if present.
+ *
+ * @param {String} text the text to strip
+ * @return {String} the last word in the input text
+ */
+function getAccountName(text) {
+    console.log(`text to parse: ${text}`);
+    const words = text.split(/[ ,]+/);
+    console.log(`words: ${words}`);
+    // TODO: fix this string ugliness later
+    const name = words.pop().replace('\"', '');
+    // Remove the trailing ? if present
+    return name.replace('?', '');
   }
