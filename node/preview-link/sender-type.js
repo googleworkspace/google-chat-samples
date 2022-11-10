1404107431419194748ba765ec50e98f078c5052
vinay-google <vinayvyas@google.com>
2022-11-10 19:09:33 +0000

    chore: Remove language from preview link region tags

--- node/preview-link/sender-type.js
+++ node/preview-link/sender-type.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// [START hangouts_chat_node_preview_link]
+// [START hangouts_chat_preview_link]
 
 /**
  * Responds to messages that have links whose URLs match URL patterns
@@ -50,4 +50,4 @@ exports.onMessage = (req, res) => {
   }
 };
 
-// [END hangouts_chat_node_preview_link]
+// [END hangouts_chat_preview_link]
