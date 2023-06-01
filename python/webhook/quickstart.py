111a62a96e2c83be90121be721edbd438ed53fb6
David Harcombe <david.harcombe@gmail.com>
2023-06-01 09:25:16 -0400

    Update to new names (#221)
    - Change references from 'Hangouts Chat' to 'Google Chat'.
    - Update `quickstart.py` to remove URL with embedded API key.
    - Update docs to add virtual environment instructions in common with
      other samples.
    - Update webhook URL.

--- python/webhook/quickstart.py
+++ python/webhook/quickstart.py
@@ -1,31 +1,27 @@
-
-"""
--*- coding: utf-8 -*-
-
-Copyright ©2020 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at apache.org/licenses/LICENSE-2.0.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-# A sample script for using an incoming webhook for Hangouts chat rooms.
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# A sample script for using an incoming webhook for Google Chat rooms.
 # [START hangouts_python_webhook]
 from json import dumps
 
 from httplib2 import Http
 
+WEBHOOK_URL = "[URL FROM WEBHOOK]"
 
 def main():
-    """Hangouts Chat incoming webhook quickstart."""
-    url = 'https://chat.googleapis.com/v1/spaces/7D8la4AAAAE/messages?key=' \
-          'AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=' \
-          'HulDviWFMz6R5FW-GwlO4INCW1cZGDDExuWVaEbbr_g%3D'
+    """Google Chat incoming webhook quickstart."""
+    url = WEBHOOK_URL
     bot_message = {
         'text': 'Hello from a Python script!'}
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
@@ -42,3 +38,4 @@ def main():
 if __name__ == '__main__':
     main()
 # [END hangouts_python_webhook]
+
