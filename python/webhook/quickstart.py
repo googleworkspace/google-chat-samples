cb56d0902d82f922b84a5bce37938d6276881d64
Moises Silva <moises.silva@gmail.com>
2024-02-04 13:01:26 -0500

    fix: Update Webhook python quickstart for consistency (#265)
    The example url doesn't contain the expected parameters which
    caused some confusion about whether they should be included or not.
    
    Also added the same comment as the thread reply example for clarity.

--- python/webhook/quickstart.py
+++ python/webhook/quickstart.py
@@ -18,10 +18,13 @@
 from json import dumps
 from httplib2 import Http
 
+# Copy the webhook URL from the Chat space where the webhook is registered.
+# The values for SPACE_ID, KEY, and TOKEN are set by Chat, and are included
+# when you copy the webhook URL.
 
 def main():
     """Google Chat incoming webhook quickstart."""
-    url = "https://chat.googleapis.com/v1/spaces/SPACE_ID/messages"
+    url = "https://chat.googleapis.com/v1/spaces/SPACE_ID/messages?key=KEY&token=TOKEN"
     app_message = {"text": "Hello from a Python script!"}
     message_headers = {"Content-Type": "application/json; charset=UTF-8"}
     http_obj = Http()
