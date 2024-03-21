4eb6ba74f9fd4c1befb020d42b6d626de23c4ed0
pierrick <pierrick@google.com>
2024-03-21 13:03:41 +0000

    Switch from Text Bison to Gemini

--- apps-script/chat-apps-gemini/issue-management-app/5-inclusivity-help/VertexAi.js
+++ apps-script/chat-apps-gemini/issue-management-app/5-inclusivity-help/VertexAi.js
@@ -17,7 +17,7 @@
 // This script contains the Google Vertex AI-specific utilities functions.
 
 /**
- * Summarizes a Google Chat space history using the text-bison model using Vertex AI API.
+ * Summarizes a Google Chat space history with the Gemini Pro model using Vertex AI API.
  *
  * @param {string} history the history
  * @return {string} the summary
@@ -25,32 +25,37 @@
 function summarizeSpace(history) {
   const url = `https://${VERTEX_AI_LOCATION_ID}-aiplatform.googleapis.com/v1`
     + `/projects/${PROJECT_ID}/locations/${VERTEX_AI_LOCATION_ID}`
-    + "/publishers/google/models/text-bison:predict";
-  const options = {
+    + "/publishers/google/models/gemini-1.0-pro:generateContent";
+  const options =  {
     method: "POST",
     headers: { Authorization: "Bearer " + ScriptApp.getOAuthToken() },
     contentType: "application/json",
     payload: JSON.stringify({
-      instances: [{
-        prompt: "Summarize the following conversation between engineers resolving an issue"
-              + " in a few sentences. Use only the information from the conversation.\n\n"
-              + history
-      }],
-      parameters: {
-        maxOutputTokens: 256,
+      contents: {
+        role: "user",
+        parts: {
+          text: "Summarize the following conversation between engineers resolving an issue"
+              + " in a few sentences.\n\n" + history
+        }
+      },
+      safetySettings: {
+        category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        threshold: "BLOCK_LOW_AND_ABOVE"
+      },
+      generationConfig: {
         temperature: 0.2,
-        topP: 0.95,
+        topP: 0.8,
         topK: 40
       }
     })
   };
 
   return JSON.parse(UrlFetchApp.fetch(url, options).getContentText())
-    .predictions[0].content;
+    .candidates[0].content.parts[0].text;
 }
 
 /**
- * Gets feedback on inclusivity for a text using the text-bison model using Vertex AI API.
+ * Gets feedback on inclusivity for a text with the Gemini Pro model using Vertex AI API.
  * 
  * Returns "It's inclusive!" when it is otherwise a list of word(s) that might not be optimal.
  *
