4177855608e0b20390e9825792b58f6316b1f7a0
Pierrick Voulet <6769971+PierrickVoulet@users.noreply.github.com>
2024-04-27 14:37:24 +0000

    fix: add semi colon in config constant statements (#278)
    * fix: add semi colon in config constant statements
    
    * Update Configuration.js
    
    * Update Configuration.js
    
    * Update Configuration.js
    
    * Update Configuration.js

--- apps-script/chat-apps-gemini/issue-management-app/1-baseline/Configuration.js
+++ apps-script/chat-apps-gemini/issue-management-app/1-baseline/Configuration.js
@@ -21,6 +21,6 @@ const CLOSE_COMMAND_ID = 2;
 const CHAT_CREDENTIALS = {
   // Replace with the Google Chat credentials to use for app authenticatio, the service account
   // private key's JSON.
-}
+};
 const PROJECT_ID = 'replace-with-gcp-project-id';
 const VERTEX_AI_LOCATION_ID = 'us-central1';
