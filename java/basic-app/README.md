6f1336812a1e5f13ea84edd1d8aa1813dcd0e72d
pierrick <pierrick@google.com>
2024-08-03 01:34:43 +0000

    feat: add request verification methods to basic app

--- java/basic-app/README.md
+++ java/basic-app/README.md
@@ -1,19 +1,19 @@
 # Google Chat basic app
 
 This code sample creates a simple Google Chat app that responds to events and
-messages from a room. The sample is built using Java 11 and 
+messages from a space. The sample is built using Java 17 and 
 [Spring Boot](https://spring.io/projects/spring-boot) on Google App Engine,
 Standard Environment.
 
 ## Deploy the sample
 
-  1. Follow the steps in [Setting Up Your Development Environment](https://cloud.google.com/appengine/docs/standard/java11/setting-up-environment)
+  1. Follow the steps in [Setting Up Your Development Environment](https://cloud.google.com/appengine/docs/standard/setting-up-environment?tab=java)
      to install Java and the Google Cloud SDK.
       
-  1. Follow the steps in [Using Maven and the App Engine Plugin](https://cloud.google.com/appengine/docs/standard/java11/using-maven)
+  1. Follow the steps in [Using Maven and the App Engine Plugin](https://cloud.google.com/appengine/docs/standard/java-gen2/using-maven)
      to install Maven.
 
-  1. Follow the steps in [Setting Up Your GCP Resources](https://cloud.google.com/appengine/docs/standard/java11/console/#create)
+  1. Follow the steps in [Setting Up Your GCP Resources](https://cloud.google.com/appengine/docs/standard/managing-projects-apps-billing#create)
      to create a project and enable App Engine.
 
   1. Run the following command to deploy the app:
@@ -28,14 +28,14 @@ Standard Environment.
      [Publishing apps](https://developers.google.com/chat/how-tos/apps-publish).
   1. When configuring the app on the **Configuration** tab on the
      **Google Chat API** page, enter the URL for the deployed version
-     of the app into the **app URL** text box.
+     of the app into the **App URL** text box.
 
 
 ## Interact with the app
 
-Either add and @mention the app in a room or in a direct mention to engage with the app.
+Either add and @mention the app in a space or in a direct mention to engage with the app.
 
-When added to a room or messaged, the app will respond with a simple reply.
+When added to a space or messaged, the app will respond with a simple reply.
 
 ## Run the sample locally
 
@@ -48,5 +48,5 @@ To verify that the sample is running and responds with the correct data
 to incoming requests, run the following command from the terminal:
 
 ```
-curl -X POST -H 'Content-Type: application/json' 'http://localhost:8080' -d '{ "type": "MESSAGE", "message": { "text": "Hello!", "sender": { "displayName": "me"}}, "space": { "displayName": "some room"}}'
+curl -X POST -H 'Content-Type: application/json' 'http://localhost:8080' -d '{ "type": "MESSAGE", "message": { "text": "Hello!", "sender": { "displayName": "me"}}, "space": { "displayName": "some space"}}'
 ```
--- java/basic-app/src/main/java/com/google/chat/app/basic/App.java
+++ java/basic-app/src/main/java/com/google/chat/app/basic/App.java
@@ -1,34 +1,48 @@
 /**
  * Copyright 2017 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package com.google.chat.app.basic;
 
-// [START basic-bot]
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.auth.oauth2.GooglePublicKeysManager;
+import com.google.api.client.http.apache.ApacheHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.chat.v1.model.Message;
+import java.util.Collections;
 import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class App {
+
+  // Authentication audience (either APP_URL or PROJECT_NUMBER)
+  static String AUDIENCE_TYPE = "APP_URL";
+
+  // Intended audience of the token:
+  // - The URL of the app when AUDIENCE_TYPE is set to APP_URL
+  // - The project number when AUDIENCE_TYPE is set to PROJECT_NUMBER
+  static String AUDIENCE = "AUDIENCE";
+
   private static final Logger logger = Logger.getLogger(App.class.getName());
 
   public static void main(String[] args) {
@@ -36,40 +50,92 @@ public class App {
   }
 
   /**
-   * Returns a simple text message app response based on the type of triggering
-   * event
+   * Returns a simple text message app response based on the type of triggering event
    *
    * @param event Event from chat.
    * @return Message
+   * @throws Exception
    */
   @PostMapping("/")
   @ResponseBody
-  public Message onEvent(@RequestBody JsonNode event) {
+  public Message onEvent(
+      @RequestBody JsonNode event, @RequestHeader("Authorization") String authorization)
+      throws Exception {
     String replyText = "";
-    switch (event.get("type").asText()) {
-      case "ADDED_TO_SPACE":
-        String spaceType = event.at("/space/type").asText();
-        if ("ROOM".equals(spaceType)) {
-          String displayName = event.at("/space/displayName").asText();
-          replyText = String.format("Thanks for adding me to %s", displayName);
-        } else {
-          String displayName = event.at("/user/displayName").asText();
-          replyText = String.format("Thanks for adding me to a DM, %s!", displayName);
-        }
-        break;
-      case "MESSAGE":
-        String message = event.at("/message/text").asText();
-        replyText = String.format("Your message: %s", message);
-        break;
-      case "REMOVED_FROM_SPACE":
-        String name = event.at("/space/name").asText();
-        logger.info(String.format("App removed from %s", name));
-        break;
-      default:
-        replyText = "Cannot determine event type";
+    if (!verifyChatAppRequest(
+        authorization.substring("Bearer ".length(), authorization.length()))) {
+      replyText = "Failed verification!";
+    } else {
+      switch (event.get("type").asText()) {
+        case "ADDED_TO_SPACE":
+          String spaceType = event.at("/space/type").asText();
+          if ("ROOM".equals(spaceType)) {
+            String displayName = event.at("/space/displayName").asText();
+            replyText = String.format("Thanks for adding me to %s", displayName);
+          } else {
+            String displayName = event.at("/user/displayName").asText();
+            replyText = String.format("Thanks for adding me to a DM, %s!", displayName);
+          }
+          break;
+        case "MESSAGE":
+          String message = event.at("/message/text").asText();
+          replyText = String.format("Your message: %s", message);
+          break;
+        case "REMOVED_FROM_SPACE":
+          String name = event.at("/space/name").asText();
+          logger.info(String.format("App removed from %s", name));
+          break;
+        default:
+          replyText = "Cannot determine event type";
+      }
     }
-    Message reply = new Message().setText(replyText);
-    return reply;
+
+    return new Message().setText(replyText);
+  }
+
+  private boolean verifyChatAppRequest(String bearer) throws Exception {
+    if ("APP_URL".equals(AUDIENCE_TYPE)) {
+      // [START chat_request_verification_app_url]
+
+      String CHAT_ISSUER = "chat@system.gserviceaccount.com";
+      JsonFactory factory = JacksonFactory.getDefaultInstance();
+
+      GoogleIdTokenVerifier verifier =
+          new GoogleIdTokenVerifier.Builder(new ApacheHttpTransport(), factory)
+              .setAudience(Collections.singletonList(AUDIENCE))
+              .build();
+
+      GoogleIdToken idToken = GoogleIdToken.parse(factory, bearer);
+      return idToken != null
+          && verifier.verify(idToken)
+          && idToken.getPayload().getEmailVerified()
+          && idToken.getPayload().getEmail().equals(CHAT_ISSUER);
+
+      // [END chat_request_verification_app_url]
+    } else if ("PROJECT_NUMBER".equals(AUDIENCE_TYPE)) {
+      // [START chat_request_verification_project_number]
+
+      String CHAT_ISSUER = "chat@system.gserviceaccount.com";
+      JsonFactory factory = JacksonFactory.getDefaultInstance();
+
+      GooglePublicKeysManager keyManagerBuilder =
+          new GooglePublicKeysManager.Builder(new ApacheHttpTransport(), factory)
+              .setPublicCertsEncodedUrl(
+                  "https://www.googleapis.com/service_accounts/v1/metadata/x509/" + CHAT_ISSUER)
+              .build();
+
+      GoogleIdTokenVerifier verifier =
+          new GoogleIdTokenVerifier.Builder(keyManagerBuilder).setIssuer(CHAT_ISSUER).build();
+
+      GoogleIdToken idToken = GoogleIdToken.parse(factory, bearer);
+      return idToken != null
+          && verifier.verify(idToken)
+          && idToken.verifyAudience(Collections.singletonList(AUDIENCE))
+          && idToken.verifyIssuer(CHAT_ISSUER);
+
+      // [END chat_request_verification_project_number]
+    }
+
+    return false;
   }
 }
-// [END basic-bot]
