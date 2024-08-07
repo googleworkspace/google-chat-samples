b0f513fc27e09b0849ebbbd8a319c5bb69a5a096
Pierrick Voulet <6769971+PierrickVoulet@users.noreply.github.com>
2024-08-07 17:55:22 -0400

    feat: add request verification methods to basic app python (#289)
    Co-authored-by: pierrick <pierrick@google.com>

--- python/basic-app/main.py
+++ python/basic-app/main.py
@@ -15,11 +15,20 @@
 # pylint: disable=invalid-name
 """
 Simple Google Chat app that responds to events and
-messages from a room.
+messages from a space.
 """
-# [START basic-bot]
 import logging
 from flask import Flask, render_template, request, json
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+# Authentication audience (either APP_URL or PROJECT_NUMBER)
+AUDIENCE_TYPE = "AUDIENCE_TYPE"
+
+# Intended audience of the token:
+# - The URL of the app when AUDIENCE_TYPE is set to APP_URL
+# - The project number when AUDIENCE_TYPE is set to PROJECT_NUMBER
+AUDIENCE = "AUDIENCE"
 
 app = Flask(__name__)
 
@@ -32,30 +41,25 @@ def home_post():
     """
 
     data = request.get_json()
+    resp_dict = format_response(data)
 
-    resp = None
-
-    if data['type'] == 'REMOVED_FROM_SPACE':
-        logging.info('App removed from a space')
-
-    else:
-        resp_dict = format_response(data)
-        resp = json.jsonify(resp_dict)
-
-    return resp
+    return json.jsonify(resp_dict)
 
 def format_response(event):
     """Determine what response to provide based upon event data.
 
     Args:
       event: A dictionary with the event data.
-
     """
 
     text = ""
 
-    # Case 1: The app was added to a room
-    if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
+    authorization = request.headers.get('Authorization')
+    if verify_chat_app_request(authorization[len("Bearer "):]) != True:
+        text = 'Failed verification!'
+
+    # Case 1: The app was added to a space
+    elif event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
         text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
 
     # Case 2: The app was added to a DM
@@ -65,9 +69,50 @@ def format_response(event):
     elif event['type'] == 'MESSAGE':
         text = 'Your message: "%s"' % event['message']['text']
 
+    elif event['type'] == 'REMOVED_FROM_SPACE':
+        logging.info('App removed from a space')
+
     return {'text': text}
 
-# [END basic-bot]
+def verify_chat_app_request(bearer):
+    """Determine whether a Google Chat request is legitimate.
+
+    Args:
+      bearer: The bearer value sent in the request.
+    """
+    if AUDIENCE_TYPE == "APP_URL":
+        # [START chat_request_verification_app_url]
+        # Bearer Tokens received by apps will always specify this issuer.
+        CHAT_ISSUER = 'chat@system.gserviceaccount.com'
+
+        try:
+            # Verify valid token, signed by CHAT_ISSUER, intended for a third party.
+            request = requests.Request()
+            token = id_token.verify_oauth2_token(bearer, request, AUDIENCE)
+            return token['email'] == CHAT_ISSUER
+
+        except:
+            return False
+        # [END chat_request_verification_app_url]
+    elif AUDIENCE_TYPE == "PROJECT_NUMBER":
+        # [START chat_request_verification_project_number]
+        # Bearer Tokens received by apps will always specify this issuer.
+        CHAT_ISSUER = 'chat@system.gserviceaccount.com'
+
+        try:
+            # Verify valid token, signed by CHAT_ISSUER, intended for a third party.
+            request = requests.Request()
+            certs_url = 'https://www.googleapis.com/service_accounts/v1/metadata/x509/' + CHAT_ISSUER
+            token = id_token.verify_token(bearer, request, AUDIENCE, certs_url)
+            return token['iss'] == CHAT_ISSUER
+
+        except:
+            return False
+        # [END chat_request_verification_project_number]
+
+    # Skip verification if AUDIENCE_TYPE is not set with supported value
+    return True;
+
 
 @app.route('/', methods=['GET'])
 def home_get():
