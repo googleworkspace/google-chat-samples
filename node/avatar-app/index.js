fb06625abd796eb292da9f011af877ee5cff7ddf
pierrick <pierrick@google.com>
2024-08-14 14:41:50 +0000

    feat: Refresh avatar app and include about slash command

--- node/avatar-app/index.js
+++ node/avatar-app/index.js
@@ -1,6 +1,5 @@
 /**
- * @license
- * Copyright Google Inc.
+ * Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,54 +13,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// [START hangouts_chat_avatar_bot]
+
+// The ID of the slash command "/about".
+// It's not enabled by default, set to the actual ID to enable it. You need to
+// use the same ID as set in the Google Chat API configuration.
+const ABOUT_COMMAND_ID = null;
+
+// [START chat_avatar_app]
 
 /**
  * Google Cloud Function that responds to messages sent from a
- * Google Chat room.
+ * Google Chat space.
  *
- * @param {Object} req Request sent from Google Chat room
+ * @param {Object} req Request sent from Google Chat space
  * @param {Object} res Response to send back
  */
-exports.helloChat = function helloChat(req, res) {
+exports.avatarApp = function avatarApp(req, res) {
   if (req.method === 'GET' || !req.body.message) {
-    res.send('Hello! This function is meant to be used in a Google Chat ' +
-      'Room.');
+    return res.send('Hello! This function is meant to be used ' +
+      'in a Google Chat Space.');
   }
+
+  // Stores the Google Chat event as a variable.
+  var event = req.body;
+
+  // [START chat_avatar_slash_command]
+  // Checks for the presence of a slash command in the message.
+  if (event.message.slashCommand) {
+    // Executes the slash command logic based on its ID.
+    // Slash command IDs are set in the Google Chat API configuration.
+    switch (event.message.slashCommand.commandId) {
+      case ABOUT_COMMAND_ID:
+        return res.send({
+          text: 'The Avatar app replies to Google Chat messages.'
+        });
+    }
+  }
+  // [END chat_avatar_slash_command]
 
   const sender = req.body.message.sender.displayName;
   const image = req.body.message.sender.avatarUrl;
-
   const data = createMessage(sender, image);
-
   res.send(data);
 };
 
 /**
  * Creates a card with two widgets.
+ * 
  * @param {string} displayName the sender's display name
  * @param {string} imageUrl the URL for the sender's avatar
  * @return {Object} a card with the user's avatar.
  */
 function createMessage(displayName, imageUrl) {
-  const cardHeader = {
-    title: `Hello ${displayName}!`,
-  };
-
-  const avatarWidget = {
-    textParagraph: {text: 'Your avatar picture: '},
-  };
-
-  const avatarImageWidget = {
-    image: {imageUrl},
-  };
-
-  const avatarSection = {
-    widgets: [
-      avatarWidget,
-      avatarImageWidget,
-    ],
-  };
+  const avatarSection = ;
 
   return {
     text: 'Here\'s your avatar',
@@ -69,10 +73,16 @@ function createMessage(displayName, imageUrl) {
       cardId: 'avatarCard',
       card: {
         name: 'Avatar Card',
-        header: cardHeader,
-        sections: [avatarSection],
+        header: {
+          title: `Hello ${displayName}!`,
+        },
+        sections: [{ widgets: [{
+          textParagraph: { text: 'Your avatar picture: ' }
+        }, {
+          image: {imageUrl}
+        }]}]
       }
-    }],
+    }]
   };
 }
-// [END hangouts_chat_avatar_bot]
+// [END chat_avatar_app]
