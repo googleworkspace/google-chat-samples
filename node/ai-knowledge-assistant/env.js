01818100f013499a2178a52509ff6bcd15cc5b95
vinay-google <vinayvyas@google.com>
2024-10-18 01:52:22 +0000

    fix: env

--- node/ai-knowledge-assistant/env.js
+++ node/ai-knowledge-assistant/env.js
@@ -19,7 +19,7 @@
  */
 const env = {
   // Replace with your GCP project ID.
-  project: 'ws-developer-017',
+  project: 'project-id',
 
   // Replace with your GCP project location. Used for Vertex AI calls.
   location: 'us-central1',
