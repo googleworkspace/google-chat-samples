7b39c2a78f3991f67ff44705ce9f9d5242f7f130
Pierrick Voulet <6769971+PierrickVoulet@users.noreply.github.com>
2025-12-02 11:10:16 -0500

    fix: Rename package from contact to selectionInput

--- java/selection-input/src/main/java/com/google/chat/selectionInput/App.java
+++ java/selection-input/src/main/java/com/google/chat/selectionInput/App.java
@@ -11,7 +11,7 @@
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.chat.contact;
+package com.google.chat.selectionInput;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
